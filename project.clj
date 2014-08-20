b77e23e2825942dd04ea052e36a79f05e52309e8
Tomek Wałkuski <ja@jestem.tw>
2014-08-20 21:30:36 +0200

    Set proper SCM metadadta

--- project.clj
+++ project.clj
@@ -3,5 +3,7 @@
   :url "https://github.com/tomekw/hikari-cp"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :scm {:name "git"
+        :url "https://github.com/tomekw/hikari-cp"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [com.zaxxer/HikariCP "2.0.1"]])
