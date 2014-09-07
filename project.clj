a72ae8974662c289dfe2d27055dd1a4c3b220b26
Tomek Wałkuski <ja@jestem.tw>
2014-09-07 17:32:17 +0200

    Set portNumber explicitly for all adapters

--- project.clj
+++ project.clj
@@ -6,4 +6,8 @@
   :scm {:name "git"
         :url "https://github.com/tomekw/hikari-cp"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [com.zaxxer/HikariCP "2.0.1"]])
+                 [com.zaxxer/HikariCP "2.0.1"]]
+  :profiles {:dev {
+                   :dependencies [[org.clojure/java.jdbc      "0.3.5"]
+                                  [mysql/mysql-connector-java "5.1.32"]
+                                  [org.postgresql/postgresql  "9.3-1102-jdbc41"]]}})
