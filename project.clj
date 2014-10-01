9b0d3e82da962c09ce7ae38217224c79d0f55cf9
Tomek Wałkuski <ja@jestem.tw>
2014-10-01 20:49:45 +0200

    Update schema and expectations

--- project.clj
+++ project.clj
@@ -7,9 +7,9 @@
         :url "https://github.com/tomekw/hikari-cp"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [com.zaxxer/HikariCP "2.0.1"]
-                 [prismatic/schema    "0.2.6"]]
+                 [prismatic/schema    "0.3.0"]]
   :profiles {:dev {
-                   :dependencies [[expectations              "2.0.9"]
+                   :dependencies [[expectations              "2.0.12"]
                                   [org.slf4j/slf4j-nop       "1.7.7"]
                                   [org.clojure/java.jdbc     "0.3.5"]
                                   [org.postgresql/postgresql "9.3-1102-jdbc41"]]
