c0d2f0c5ecb03ab5519f90dc116409f8b13974c4
Tomek Wałkuski <ja@jestem.tw>
2014-10-08 20:04:29 +0200

    Version 0.8.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject hikari-cp "0.7.0"
+(defproject hikari-cp "0.8.0"
   :description "A Clojure wrapper to HikariCP JDBC connection pool"
   :url "https://github.com/tomekw/hikari-cp"
   :license {:name "Eclipse Public License"
