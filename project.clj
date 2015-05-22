e7dd5c75e9b291635417aff2a90fd1848ea49ce7
Tomek Wałkuski <ja@jestem.tw>
2015-05-22 11:33:17 +0200

    Update HikariCP to 2.3.8

--- project.clj
+++ project.clj
@@ -7,12 +7,12 @@
         :url "https://github.com/tomekw/hikari-cp"}
   :dependencies [[org.clojure/clojure         "1.6.0"]
                  [org.tobereplaced/lettercase "1.0.0"]
-                 [com.zaxxer/HikariCP         "2.3.7"]
-                 [prismatic/schema            "0.4.1"]]
+                 [com.zaxxer/HikariCP         "2.3.8"]
+                 [prismatic/schema            "0.4.2"]]
   :profiles {:dev {
                    :dependencies [[expectations               "2.1.1"]
                                   [org.slf4j/slf4j-nop        "1.7.12"]
-                                  [org.clojure/java.jdbc      "0.3.6"]
+                                  [org.clojure/java.jdbc      "0.3.7"]
                                   [mysql/mysql-connector-java "5.1.35"]
                                   [org.postgresql/postgresql  "9.3-1102-jdbc41"]]
                    :plugins [[lein-expectations "0.0.7"]]}}
