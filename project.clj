9d04bbe55b253664c57bc39c17bf3ba6ddf68966
Jan Stępień <jstepien@users.sourceforge.net>
2016-05-18 12:15:26 +0200

    Bump to 1.6.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject hikari-cp "1.6.1"
+(defproject hikari-cp "1.6.2-SNAPSHOT"
   :description "A Clojure wrapper to HikariCP JDBC connection pool"
   :url "https://github.com/tomekw/hikari-cp"
   :license {:name "Eclipse Public License"
