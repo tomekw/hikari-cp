a51b6b53168900d5f5416b1e5c9f768ec47dc348
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2016-09-21 20:06:34 -0400

    updated to latest HikariCP
    a number of fixes since the current version, including some memory leaks

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
         :url "https://github.com/tomekw/hikari-cp"}
   :dependencies [[org.clojure/clojure         "1.7.0"]
                  [org.tobereplaced/lettercase "1.0.0"]
-                 [com.zaxxer/HikariCP         "2.4.7"]
+                 [com.zaxxer/HikariCP         "2.5.1"]
                  [prismatic/schema            "1.0.4"]]
   :profiles {:dev {
                    :dependencies [[expectations               "2.1.4"]
