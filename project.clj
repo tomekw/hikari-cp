00d069b92950576f27c70c1830518fa726d3017e
Tomek Wałkuski <contact@tomekw.com>
2019-06-29 15:27:18 +0200

    Update dev dependencies

--- project.clj
+++ project.clj
@@ -13,11 +13,11 @@
                    :dependencies [[expectations "2.1.10"]
                                   [org.slf4j/slf4j-nop "1.7.26"]
                                   [org.clojure/java.jdbc "0.7.9"]
-                                  [mysql/mysql-connector-java "8.0.15"]
+                                  [mysql/mysql-connector-java "8.0.16"]
                                   [org.neo4j/neo4j-jdbc-driver "3.4.0"]
-                                  [org.postgresql/postgresql "42.2.5"]
-                                  [io.dropwizard.metrics/metrics-core "4.0.5"]
-                                  [io.dropwizard.metrics/metrics-healthchecks "4.0.5"]
+                                  [org.postgresql/postgresql "42.2.6"]
+                                  [io.dropwizard.metrics/metrics-core "4.1.0"]
+                                  [io.dropwizard.metrics/metrics-healthchecks "4.1.0"]
 
                                   ; The Oracle driver is only accessible from maven.oracle.com
                                   ; which requires a userId and password
