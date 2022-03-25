900a0f31f937791fc3963ba56c99e7bd07e6909b
Tomek Wałkuski <contact@tomekw.com>
2022-03-25 09:25:13 +0100

    Version 2.14.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,7 @@
 ## unreleased
 
+## 2.14.0
+
 * updated `HikariCP` to `4.0.3`
 
 ## 2.13.0
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject hikari-cp "2.13.0"
+(defproject hikari-cp "2.14.0"
   :description "A Clojure wrapper to HikariCP JDBC connection pool"
   :url "https://github.com/tomekw/hikari-cp"
   :license {:name "Eclipse Public License"
